b87410225ca609f5c0989b4b834ee82b12cabde7
amirfzd <amiralifm1407@gmail.com>
2025-09-11 19:42:33 +0330

    fix: fix issue on reading merchantId from request body

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
+from bson import ObjectId
+
+class ObjectIdField(serializers.Field):
+    def to_representation(self, value):
+        return str(value) 
+
+    def to_internal_value(self, data):
+        try:
+            return ObjectId(str(data))
+        except Exception:
+            raise serializers.ValidationError("Invalid ObjectId")
+    
 
 class ReportQuerySerializer(serializers.Serializer):
     type = serializers.ChoiceField(choices=['count', 'amount'])
     mode = serializers.ChoiceField(choices=['daily', 'weekly', 'monthly'])
-    merchantId = serializers.CharField(required=False, allow_blank=True,)
+    merchantId =ObjectIdField(required=False)
